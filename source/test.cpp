d4d08fb59436068d9f101c88bc3818715778ab78
NishikiChisato <1484625730@qq.com>
2023-03-12 09:44:40 +0800

    Test

--- source/test.cpp
+++ source/test.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 
 #include "alloc.h"
+#include "iterator.h"
+
+void print(mystl::__true_type)
+{
+    std::cout << "YES" << std::endl;
+}
 
 int main()
 {
-    mystl::alloc al;
-    int * p = static_cast<int*>(al.allocate(10 * 4));
-    for(int i = 0; i < 10; i ++)
-        *(p + i) = i;
-    for(int i = 0; i < 10; i ++)
-        std::cout << *(p +i) << " ";
-    std::cout << std::endl;
-
-    al.deallocate(p, 4 * 10);
-    std::cout << "Success" << std::endl;
+    print(mystl::__type_traits<int>::is_POD_type());
 
     return 0;
 }
