cb65e87cb5cb7d0f42e86452e35c93e39c64a51f
NishikiChisato <1484625730@qq.com>
2023-03-13 10:40:55 +0800

    Test

--- source/test.cpp
+++ source/test.cpp
@@ -3,6 +3,8 @@
 #include "alloc.h"
 #include "iterator.h"
 
+#include <vector>
+
 void print(mystl::__true_type)
 {
     std::cout << "YES" << std::endl;
@@ -11,6 +13,16 @@ void print(mystl::__true_type)
 int main()
 {
     print(mystl::__type_traits<int>::is_POD_type());
+
+    std::vector<int>v = {1,2,3,4,5,6,7,8,9};
+
+    std::vector<int>::iterator it = v.begin();
+
+    std::cout << *it << std::endl;
+
+    mystl::advance(it, 2);
+
+    std::cout << *it << std::endl;
 
     return 0;
 }
